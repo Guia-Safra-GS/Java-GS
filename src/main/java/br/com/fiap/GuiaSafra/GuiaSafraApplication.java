af3bd41f075c2d04f4c81a61b034b9c58dc6d3ff
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-05-28 17:57:03 -0300

    feat: started the repo with basic structure.

---
+++ src/main/java/br/com/fiap/GuiaSafra/GuiaSafraApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.GuiaSafra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaSafraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaSafraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/GuiaSafra/GuiaSafraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.GuiaSafra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaSafraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
